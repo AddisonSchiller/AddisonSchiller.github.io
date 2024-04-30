a4d8c74496308d0d65785b2e8c8b68daa476e00c
Addison Schiller <aschiller@axon.com>
2024-04-30 16:04:08 -0400

    f

--- src/App.tsx
+++ src/App.tsx
@@ -1,218 +1,193 @@
-import React from 'react'
-import { sortedUniq, keys, uniq } from 'lodash'
-import prettier from 'prettier'
-import parserBabel from 'prettier/parser-babel'
-import CodeMirror from '@uiw/react-codemirror'
-import { json } from '@codemirror/lang-json'
-import {walkJsonSchema7} from './utils'
-
-
-
+import React from "react";
+import { sortedUniq, keys, uniq, sortBy } from "lodash";
+import prettier from "prettier";
+import parserBabel from "prettier/parser-babel";
+import CodeMirror from "@uiw/react-codemirror";
+import { json } from "@codemirror/lang-json";
+import { walkJsonSchema7, deepCloneAndMergeLeft } from "./utils";
 
 interface HashValue {
-  schema1?: string
-  schema2?: string
-  diffs?: string
+  schema1?: string;
+  schema2?: string;
+  diffs?: string;
 }
 
 function decode(hash: string): HashValue {
-  const parsed = JSON.parse(decodeURIComponent(atob(hash)))
+  const parsed = JSON.parse(decodeURIComponent(atob(hash)));
 
-  const formattedSchema1 = prettier.format(parsed.schema1 || '', {
-    parser: 'json',
+  const formattedSchema1 = prettier.format(parsed.schema1 || "", {
+    parser: "json",
     plugins: [parserBabel],
-  })
+  });
 
-  const formattedSchema2 = prettier.format(parsed.schema2 || '', {
-    parser: 'json',
+  const formattedSchema2 = prettier.format(parsed.schema2 || "", {
+    parser: "json",
     plugins: [parserBabel],
-  })
+  });
 
-  return { schema1: formattedSchema1, schema2: formattedSchema2 }
+  return { schema1: formattedSchema1, schema2: formattedSchema2 };
 }
 
 function getHash(): HashValue {
-  const hash = window.location.hash.substring(1)
-  if (!hash) return {}
+  const hash = window.location.hash.substring(1);
+  if (!hash) return {};
 
-  let decoded
+  let decoded;
 
   try {
-    decoded = decode(hash)
+    decoded = decode(hash);
   } catch (e) {
     decoded = {
-      schema1: '',
-      schema2: '',
-    }
+      schema1: "",
+      schema2: "",
+    };
   }
 
   return {
     schema1: decoded.schema1,
     schema2: decoded.schema2,
-  }
+  };
 }
 
 function encode(value: any): string {
-  const stringValue = JSON.stringify(value)
-  const trimmedValue = stringValue.replace(/\s{2,}/g, ' ').replace(/\r\n/g, '')
-  return btoa(encodeURIComponent(trimmedValue))
+  const stringValue = JSON.stringify(value);
+  const trimmedValue = stringValue.replace(/\s{2,}/g, " ").replace(/\r\n/g, "");
+  return btoa(encodeURIComponent(trimmedValue));
 }
 
 const updateHash = ({ schema1, schema2 }: HashValue) => {
-  window.location.hash = encode({ schema1, schema2 })
-}
+  window.location.hash = encode({ schema1, schema2 });
+};
 
 export function App() {
-  const [schema1, setSchema1] = React.useState<string>(getHash().schema1 || '')
-  const [schema2, setSchema2] = React.useState<string>(getHash().schema2 || '')
-  const [diffs, setDiffs] = React.useState<string>(getHash().diffs || '')
+  const [schema1, setSchema1] = React.useState<string>(getHash().schema1 || "");
+  const [schema2, setSchema2] = React.useState<string>(getHash().schema2 || "");
+  const [diffs, setDiffs] = React.useState<string>(getHash().diffs || "");
 
   const format = () => {
     const formattedSchema1 = prettier.format(schema1, {
-      parser: 'json',
+      parser: "json",
       plugins: [parserBabel],
-    })
+    });
     const formattedSchema2 = prettier.format(schema2, {
-      parser: 'json',
+      parser: "json",
       plugins: [parserBabel],
-    })
+    });
 
-    updateHash({ schema1: formattedSchema1, schema2: formattedSchema2 })
-    setSchema2(formattedSchema2)
-    setSchema1(formattedSchema1)
-  }
+    updateHash({ schema1: formattedSchema1, schema2: formattedSchema2 });
+    setSchema2(formattedSchema2);
+    setSchema1(formattedSchema1);
+  };
 
-  const diffSchemas = ()=>{
-    const schema1Map: any = {}
-    const schema2Map: any = {}
-    walkJsonSchema7({dataSchema:JSON.parse(schema1), walk: ({dataRef, subschema}:{dataRef:string, subschema: any}): boolean=>{
-      schema1Map[dataRef] = subschema
-      return true
-    }})
+  const diffSchemas = () => {
+    const schema1Map: any = {};
+    const schema2Map: any = {};
+    walkJsonSchema7({
+      dataSchema: JSON.parse(schema1),
+      walk: ({
+        dataRef,
+        subschema,
+      }: {
+        dataRef: string;
+        subschema: any;
+      }): boolean => {
+        schema1Map[dataRef] = subschema;
+        return true;
+      },
+    });
 
-    walkJsonSchema7({dataSchema:JSON.parse(schema2), walk: ({dataRef, subschema}:{dataRef:string, subschema: any}): boolean=>{
-      schema2Map[dataRef] = subschema
-      
-      
-      return true
-    }})
-    const uniqueKeys = uniq([...keys(schema1Map), ...keys(schema2Map)])
-    const schemaDiffs: string[] = []
-    uniqueKeys.forEach(dataRef =>{
-      const s1 = schema1Map[dataRef]
-      const s2 = schema2Map[dataRef]
-      if(!s1){
-        schemaDiffs.push(`missing dataRef ${dataRef} in first schema`)
-        return
+    walkJsonSchema7({
+      dataSchema: JSON.parse(schema2),
+      walk: ({
+        dataRef,
+        subschema,
+      }: {
+        dataRef: string;
+        subschema: any;
+      }): boolean => {
+        schema2Map[dataRef] = subschema;
+
+        return true;
+      },
+    });
+    const uniqueKeys = uniq([...keys(schema1Map), ...keys(schema2Map)]);
+    const schemaDiffs: string[] = [];
+    uniqueKeys.forEach((dataRef) => {
+      const s1 = schema1Map[dataRef];
+      const s2 = schema2Map[dataRef];
+      if (!s1) {
+        schemaDiffs.push(`missing dataRef ${dataRef} in first schema`);
+        return;
       }
-      if(!s2){
-        schemaDiffs.push(`missing dataRef ${dataRef} in second schema`)
-        return
+      if (!s2) {
+        schemaDiffs.push(`missing dataRef ${dataRef} in second schema`);
+        return;
       }
 
-      if(JSON.stringify(s1.type) !== JSON.stringify(s2.type)){
-        
-        schemaDiffs.push(`${dataRef} has conflicting types: ${s1.type} and ${s2.type}`)
-        
+      if (JSON.stringify(s1.type) !== JSON.stringify(s2.type)) {
+        schemaDiffs.push(
+          `${dataRef} has conflicting types: ${s1.type} and ${s2.type}`
+        );
       }
-      if((s1.enum && !s2.enum) || (s2.enum && !s1.enum)){
-        schemaDiffs.push(`${dataRef} has conflicting types. One is an Enum and one is not`)
-        return
+      if ((s1.enum && !s2.enum) || (s2.enum && !s1.enum)) {
+        schemaDiffs.push(
+          `${dataRef} has conflicting types. One is an Enum and one is not`
+        );
+        return;
       }
-      if(s1.enum && s2.enum){
-        if(JSON.stringify(sortedUniq(s1.enum)) !== JSON.stringify(sortedUniq(s2.enum))){
-          schemaDiffs.push(`${dataRef} differing enums`)
+      if (s1.enum && s2.enum) {
+        if (
+          JSON.stringify(sortedUniq(sortBy(s1.enum))) !==
+          JSON.stringify(sortedUniq(sortBy(s2.enum)))
+        ) {
+          schemaDiffs.push(`${dataRef} differing enums`);
         }
       }
+    });
 
-    })
+    setDiffs(schemaDiffs.join("\n"));
+  };
 
-    setDiffs(schemaDiffs.join('\n'))    
-    }
+  const mergeSchemas = () => {
+    const clonedSchema = deepCloneAndMergeLeft(schema1, schema2);
 
-    const mergeSchemas = ()=>{
-      const schema1Map: any = {}
-      const schema2Map: any = {}
-      walkJsonSchema7({dataSchema:JSON.parse(schema1), walk: ({dataRef, subschema}:{dataRef:string, subschema: any}): boolean=>{
-        schema1Map[dataRef] = subschema
-        return true
-      }})
-  
-      walkJsonSchema7({dataSchema:JSON.parse(schema2), walk: ({dataRef, subschema}:{dataRef:string, subschema: any}): boolean=>{
-        schema2Map[dataRef] = subschema
-        
-        
-        return true
-      }})
-      const uniqueKeys = uniq([...keys(schema1Map), ...keys(schema2Map)])
-      const schemaDiffs: string[] = []
-      uniqueKeys.forEach(dataRef =>{
-        const s1 = schema1Map[dataRef]
-        const s2 = schema2Map[dataRef]
-        if(!s1){
-          schemaDiffs.push(`missing dataRef ${dataRef} in first schema`)
-          return
-        }
-        if(!s2){
-          schemaDiffs.push(`missing dataRef ${dataRef} in second schema`)
-          return
-        }
-  
-        if(JSON.stringify(s1.type) !== JSON.stringify(s2.type)){
-          
-          schemaDiffs.push(`${dataRef} has conflicting types: ${s1.type} and ${s2.type}`)
-          
-        }
-        if((s1.enum && !s2.enum) || (s2.enum && !s1.enum)){
-          schemaDiffs.push(`${dataRef} has conflicting types. One is an Enum and one is not`)
-          return
-        }
-        if(s1.enum && s2.enum){
-          if(JSON.stringify(sortedUniq(s1.enum)) !== JSON.stringify(sortedUniq(s2.enum))){
-            schemaDiffs.push(`${dataRef} differing enums`)
-          }
-        }
-  
+    setDiffs(
+      prettier.format(JSON.stringify(clonedSchema) || "", {
+        parser: "json",
+        plugins: [parserBabel],
       })
-  
-      setDiffs(schemaDiffs.join('\n'))    
-      }
-  
-
+    );
+  };
 
   const handleBlur = () => {
-    updateHash({ schema1, schema2 })
-  }
+    updateHash({ schema1, schema2 });
+  };
 
-  let schema1Json: any = []
-  let schema1Error = false
+  let schema1Json: any = [];
+  let schema1Error = false;
 
-  let schema2JSON: any
-  let schema2Error = false
-
-  try {
-    schema2JSON = JSON.parse(schema2)
-  } catch (e) {
-    schema2Error = true
-    schema2JSON = {}
-  }
+  let schema2JSON: any;
+  let schema2Error = false;
 
   try {
-    schema1Json = JSON.parse(schema1)
+    schema2JSON = JSON.parse(schema2);
   } catch (e) {
-    schema1Error = true
-    schema1Json = []
+    schema2Error = true;
+    schema2JSON = {};
   }
 
-  
-
-
+  try {
+    schema1Json = JSON.parse(schema1);
+  } catch (e) {
+    schema1Error = true;
+    schema1Json = [];
+  }
 
   return (
     <div>
-      <div style={{ display: 'flex' }}>
+      <div style={{ display: "flex" }}>
         <div>
-          <label style={{ display: 'block' }} htmlFor="schema1">
+          <label style={{ display: "block" }} htmlFor="schema1">
             schema1
           </label>
           <CodeMirror
@@ -224,14 +199,14 @@ export function App() {
             height="40em"
             extensions={[json()]}
             style={{
-              border: '1px solid black',
-              resize: 'both',
-              overflow: 'auto !important',
+              border: "1px solid black",
+              resize: "both",
+              overflow: "auto !important",
             }}
           />
         </div>
         <div>
-          <label style={{ display: 'block' }} htmlFor="schema2">
+          <label style={{ display: "block" }} htmlFor="schema2">
             schema2
           </label>
           <CodeMirror
@@ -243,56 +218,56 @@ export function App() {
             onBlur={handleBlur}
             value={schema2}
             style={{
-              border: '1px solid black',
-              resize: 'both',
-              overflow: 'auto !important',
+              border: "1px solid black",
+              resize: "both",
+              overflow: "auto !important",
             }}
           />
         </div>
         <button
-          style={{ alignSelf: 'flex-start', marginTop: 16, marginLeft: 16 }}
+          style={{ alignSelf: "flex-start", marginTop: 16, marginLeft: 16 }}
           onClick={format}
         >
           Format
         </button>
 
         <button
-          style={{ alignSelf: 'flex-start', marginTop: 16, marginLeft: 16 }}
+          style={{ alignSelf: "flex-start", marginTop: 16, marginLeft: 16 }}
           onClick={diffSchemas}
         >
           Diff
         </button>
 
         <button
-          style={{ alignSelf: 'flex-start', marginTop: 16, marginLeft: 16 }}
+          style={{ alignSelf: "flex-start", marginTop: 16, marginLeft: 16 }}
           onClick={mergeSchemas}
         >
-          Diff
+          Create Merged Schema
         </button>
       </div>
 
-      {schema2Error && <p style={{ color: 'red' }}>Error processing schema2</p>}
-      {schema1Error && <p style={{ color: 'red' }}>Error processing schema1</p>}
+      {schema2Error && <p style={{ color: "red" }}>Error processing schema2</p>}
+      {schema1Error && <p style={{ color: "red" }}>Error processing schema1</p>}
 
       <div>
-          <label style={{ display: 'block' }} htmlFor="diffs">
-            Diffs/Merged Schema
-          </label>
-          <CodeMirror
-            id="diffs"
-            readOnly={true}
-            width="1200px"
-            height="40em"
-            extensions={[json()]}
-            onBlur={handleBlur}
-            value={diffs}
-            style={{
-              border: '1px solid black',
-              resize: 'both',
-              overflow: 'auto !important',
-            }}
-          />
-        </div>
+        <label style={{ display: "block" }} htmlFor="diffs">
+          Diffs/Merged Schema
+        </label>
+        <CodeMirror
+          id="diffs"
+          readOnly={true}
+          width="1200px"
+          height="40em"
+          extensions={[json()]}
+          onBlur={handleBlur}
+          value={diffs}
+          style={{
+            border: "1px solid black",
+            resize: "both",
+            overflow: "auto !important",
+          }}
+        />
+      </div>
     </div>
-  )
+  );
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 /* eslint-disable complexity */
-import { forEach } from "lodash";
+import { forEach, cloneDeep, sortedUniq, sortBy, merge } from "lodash";
 
 function joinPaths(...paths: Array<string | number>): string {
   return paths
@@ -34,7 +34,7 @@ export function walkJsonSchema7({
     return;
   }
 
-  const { items: itemSchema, properties, oneOf, type: schemaType } = dataSchema;
+  const { items: itemSchema, properties, type: schemaType } = dataSchema;
 
   const shouldWalkChildren = walk({
     dataRef,
@@ -64,15 +64,60 @@ export function walkJsonSchema7({
       relationType,
       walk,
     });
-  } else if (schemaType === "object" && oneOf) {
-    forEach(oneOf, (propertySchema: any) => {
-      walkJsonSchema7({
-        dataRef: joinPaths(dataRef),
-        dataSchema: propertySchema,
-        entityType,
-        relationType,
-        walk,
-      });
-    });
   }
+}
+
+function doubleWalkerMerger(schema1: any, schema2: any, dataRef?: string) {
+  if (!schema1 || !schema2) {
+    return;
+  }
+  const {
+    items: items1,
+    properties: properties1,
+    type: schemaType1,
+    enum: enum1,
+  } = schema1;
+  const {
+    items: items2,
+    properties: properties2,
+    type: schemaType2,
+    enum: enum2,
+  } = schema2;
+
+  if (!items1 && !properties1 && !schemaType1 && !enum1) {
+    merge(schema1, schema2);
+    return;
+  }
+  if (schemaType1 && schemaType2 && schemaType1 !== schemaType2) {
+    return;
+  }
+
+  if (enum1 && enum2) {
+    schema1.enum = sortedUniq(sortBy([...enum1, ...enum2]));
+    merge(schema1.$enumMeta, schema2.$enumMeta);
+    return;
+  }
+
+  if (schemaType2 === "object" && properties2) {
+    forEach(properties2, (propertySchema: any, propertyName: string) => {
+      if (!properties1[propertyName]) {
+        properties1[propertyName] = propertySchema;
+      } else {
+        doubleWalkerMerger(
+          properties1[propertyName],
+          propertySchema,
+          joinPaths(dataRef || "", propertyName)
+        );
+      }
+    });
+  } else if (schemaType2 === "array" && items2) {
+    doubleWalkerMerger(items1, items2, joinPaths(dataRef || "", "*"));
+  }
+}
+
+export function deepCloneAndMergeLeft(schema1: any, schema2: any): any {
+  const clonedSchema = cloneDeep(JSON.parse(schema1));
+  doubleWalkerMerger(clonedSchema, JSON.parse(schema2));
+
+  return clonedSchema;
 }
